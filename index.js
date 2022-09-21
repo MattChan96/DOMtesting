dfda382f236cb0beec8eed7af915e7626a15b72c
ahmed <ahmedmuhammed78613@gmail.com>
2022-09-21 12:19:08 +0100

    h2 js function

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-const hello = require('./hello');
+const hello = require("./hello");
 
 hello();
 console.log("We love LOTR");
 
 const heading = document.querySelector("h1");
-console.log(heading)
+console.log(heading);
 
 heading.textContent = "The LOR - by Gustafsson";
 heading.style.color = "purple";
 
 const para = document.querySelectorAll("p");
-para.forEach(para => para.style.color = "green");
+para.forEach((para) => (para.style.color = "green"));
 
-document.querySelector('#num1').textContent = "Gimli";
+document.querySelector("#num1").textContent = "Gimli";
 
-document.getElementById('num3').style.color = "yellow";
+document.getElementById("num3").style.color = "yellow";
 
 // const rando = document.getElementsByClassName('rando');
 // console.log(rando);
 
 // document.getElementsByTagName('li').style.textDecoration = "underline"
 
-const buttonAdd = document.getElementById('addCharacter');
+const buttonAdd = document.getElementById("addCharacter");
 console.log(buttonAdd);
 
-buttonAdd.addEventListener('click', () => {
-    const ol = document.querySelector("ol")
-    const li = document.createElement("li")
-    li.textContent = "Gandalf";
-    ol.appendChild(li)
-})
+buttonAdd.addEventListener("click", () => {
+	const ol = document.querySelector("ol");
+	const li = document.createElement("li");
+	li.textContent = "Gandalf";
+	ol.appendChild(li);
+});
 
 const ol = document.querySelector("ol");
 ol.addEventListener("mouseover", () => {
-    console.log(ol);
-    ol.style.color = "red"
-})
+	console.log(ol);
+	ol.style.color = "red";
+});
 
 const olblack = document.querySelector("ol");
 ol.addEventListener("mouseleave", () => {
-    console.log(ol);
-    ol.style.color = "black"
-})
+	console.log(ol);
+	ol.style.color = "black";
+});
 
-const buttonUpdate = document.querySelector('#updateButton')
+const buttonUpdate = document.querySelector("#updateButton");
 
-buttonUpdate.addEventListener('click', () => {
-    document.querySelector('h1').textContent = "you hit me";
-})
+buttonUpdate.addEventListener("click", () => {
+	document.querySelector("h1").textContent = "you hit me";
+});
+
+// h2
+
+const h2 = document.querySelectorAll("h2");
+h2.forEach((title) =>
+	title.addEventListener("mouseover", () => {
+		title.textContent = "Hello There!";
+	})
+);
